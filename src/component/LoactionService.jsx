284242cfbc717563daebdcebd35e0dcc345da07d
Jenny-shendre <jennifershendre73@gmail.com>
2024-07-11 17:23:32 +0530

    Resolved Merged Coflict

--- src/component/LoactionService.jsx
+++ src/component/LoactionService.jsx
@@ -71,21 +71,19 @@ function LoactionService() {
       setError("type", { type: "manual", message: "This field is required" });
     }
     if (!formData.projectName) {
-      setError("projectName", {
-        type: "manual",
-        message: "This field is required",
-      });
+      setError("projectName", { type: "manual", message: "This field is required" });
     }
     if (!formData.type || !formData.projectName) {
       return;
     }
 
-    const completeData = { ...formData, ...Slider.Slider };
+    const completeData = { ...data, ...formData, ...Slider.Slider[0] };
     console.log("object", completeData);
 
     try {
+
       const response = await axios.post(
-        "https://project-rof.vercel.app/api/seviceRequest",
+        "https://prodictivity-management-tool2.vercel.app/api/seviceRequest",
         completeData
       );
       console.log("Your message has been sent", response);
@@ -101,7 +99,7 @@ function LoactionService() {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://project-rof.vercel.app/api/projects"
+          "https://prodictivity-management-tool2.vercel.app/api/projects"
         );
         setData(response.data);
       } catch (error) {
@@ -115,7 +113,7 @@ function LoactionService() {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://project-rof.vercel.app/api/services/fetch-all"
+          "https://prodictivity-management-tool2.vercel.app/api/services/fetch-all"
         );
         setseviceRequest(response.data);
       } catch (error) {
@@ -127,50 +125,40 @@ function LoactionService() {
 
   return (
     <>
-      <div
-        className="opImg"
-        style={{ backgroundColor: "rgba(218, 203, 187, 0.7)" }}>
+      <div className="opImg" style={{ backgroundColor: 'rgba(218, 203, 187, 0.7)' }}>
         <div>
-          <img
-            className="h-full fixed w-full lg:opacity-[25%] md:opacity-[25%] sm:opacity-[40%] sm:bg-[#c49f82] backimg"
-            src={img}
-            alt="Background"
-          />
+          <img className="h-full fixed w-full lg:opacity-[25%] md:opacity-[25%] sm:opacity-[40%] sm:bg-[#c49f82] backimg" src={img} alt="Background" />
         </div>
 
         <Link to="/ServiceRequestForm">
-          <div className="fixed arrowss bottom-4 left-4">
+          <div className="fixed w-[64px] h-[64px] arrowss bottom-4 left-4">
             <img
-              className="lg:mt-[500px] lg:ml-12 cursor-pointer"
+              className=" cursor-pointer"
               src={Frame}
               alt="Back"
             />
           </div>
         </Link>
         <div className="opacity-100 min-h-screen flex items-center justify-center font-['Roboto'] bg-[#DACBBB]">
-          <div className="bg-[#FFFFFF99] bg-opacity-90 rounded-lg shadow-lg z-[1] p-8 w-full max-w-md">
+          <div className="bg-[#FFFFFF99] bg-opacity-90 rounded-lg shadow-lg z-[1] px-6 w-[514px] h-[464px] flex flex-col items-center">
             <div className="flex flex-col items-center">
-              <img src={Logo} alt="Logo" className="logo w-56 h-44" />{" "}
-              {/* Adjusted logo size */}
+              <img src={Logo} alt="Logo" className="logo w-[168px] h-[151px]" /> {/* Adjusted logo size */}
             </div>
 
-            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-              <div ref={typeDropdownRef}>
+            <form className="space-y-4 w-full px-6" onSubmit={handleSubmit(onSubmit)}>
+              <div ref={typeDropdownRef} className="w-full">
                 <label
                   htmlFor="type"
-                  className="block text-sm font-700 text-brown-700 font-Manrope">
+                  className="block input-fonts font-Manrope">
                   Type of Service
                 </label>
                 <div
-                  className="relative bg-white mt-1 font-Manrope block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50"
-                  onClick={() => setIsTypeDropdownOpen(!isTypeDropdownOpen)}>
+                  className="relative bg-white mt-1 font-Manrope text-[18px] font-500 text-[#000000] block input-fields shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50"
+                  onClick={() => setIsTypeDropdownOpen(!isTypeDropdownOpen)}
+                >
                   <div className="cursor-pointer flex justify-between items-center">
                     {formData.type || "Choose Services"}
-                    <img
-                      className="DropIcon ml-2"
-                      src={Drop}
-                      alt="Dropdown Icon"
-                    />
+                    <img className="DropIcon ml-2" src={Drop} alt="Dropdown Icon" />
                   </div>
                   {isTypeDropdownOpen && (
                     <div className="absolute font-Manrope select-menu z-10 mt-2 w-full bg-white border border-gray-300 rounded-md shadow-lg max-h-40 overflow-y-auto">
@@ -178,36 +166,30 @@ function LoactionService() {
                         <div
                           key={service.serviceType}
                           className="p-2 cursor-pointer hover:bg-gray-200"
-                          onClick={() => handleTypeChange(service.serviceType)}>
+                          onClick={() => handleTypeChange(service.serviceType)}
+                        >
                           {service.serviceType}
                         </div>
                       ))}
                     </div>
                   )}
                 </div>
-                {errors.type && (
-                  <span className="text-red-600">{errors.type.message}</span>
-                )}
+                {errors.type && <span className="text-red-700">{errors.type.message}</span>}
               </div>
 
-              <div ref={projectDropdownRef}>
+              <div ref={projectDropdownRef} className="w-full">
                 <label
                   htmlFor="projectName"
-                  className="block text-sm font-700 text-brown-700 font-Manrope">
+                  className="block input-fonts font-Manrope">
                   Project Name
                 </label>
                 <div
-                  className="relative bg-white mt-1 font-Manrope block w-full rounded-md border-gray-300 p-2 shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50"
-                  onClick={() =>
-                    setIsProjectDropdownOpen(!isProjectDropdownOpen)
-                  }>
+                  className="relative bg-white mt-1 font-Manrope text-[18px] font-500 text-[#000000] block input-fields shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50"
+                  onClick={() => setIsProjectDropdownOpen(!isProjectDropdownOpen)}
+                >
                   <div className="cursor-pointer flex justify-between items-center">
                     {formData.projectName || "Select a project"}
-                    <img
-                      className="DropIcon  ml-2"
-                      src={Drop}
-                      alt="Dropdown Icon"
-                    />
+                    <img className="DropIcon  ml-2" src={Drop} alt="Dropdown Icon" />
                   </div>
                   {isProjectDropdownOpen && (
                     <div className="absolute font-Manrope select-menu z-10 mt-2 w-full bg-white border border-gray-300 rounded-md shadow-lg max-h-40 overflow-y-auto">
@@ -215,24 +197,21 @@ function LoactionService() {
                         <div
                           key={project.name}
                           className="p-2 cursor-pointer hover:bg-gray-200"
-                          onClick={() => handleProjectChange(project.name)}>
+                          onClick={() => handleProjectChange(project.name)}
+                        >
                           {project.name}
                         </div>
                       ))}
                     </div>
                   )}
                 </div>
-                {errors.projectName && (
-                  <span className="text-red-600">
-                    {errors.projectName.message}
-                  </span>
-                )}
+                {errors.projectName && <span className="text-red-700">{errors.projectName.message}</span>}
               </div>
 
-              <div className="p-5">
+              <div className=" w-full">
                 <button
                   type="submit"
-                  className="font-Manrope font-700 w-full bg-[#632E04] text-white py-2 px-4 rounded-md hover:bg-brown-700 focus:outline-none focus:ring-2 focus:ring-brown-500 focus:ring-opacity-50 transition duration-150 ease-in-out">
+                  className="font-Manrope ProceedforStep2 mt-7 sticky bg-[#632E04] text-white  hover:bg-brown-700 focus:outline-none focus:ring-2 focus:ring-brown-500 focus:ring-opacity-50 transition duration-150 ease-in-out">
                   Assign Service Person
                 </button>
               </div>
--- src/component/ServiceRequestForm.jsx
+++ src/component/ServiceRequestForm.jsx
@@ -50,94 +50,101 @@ function ServiceRequestForm() {
 
   return (
     <>
-      <div className="opImg" style={{ backgroundColor: "rgba(218, 203, 187, 0.7)" }}>
+      <div className="opImg" style={{ backgroundColor: 'rgba(218, 203, 187, 0.7)' }}>
         <div>
           <img className="h-full fixed w-full lg:opacity-[25%] md:opacity-[25%] sm:opacity-[40%] sm:bg-[#c49f82] backimg" src={img} alt="Background" />
         </div>
         <Link to="/">
-          <div className="fixed bottom-4 left-4 lg:bottom-6 lg:left-6">
+          <div className="fixed bottom-4 w-[64px] h-[64px] left-4 lg:bottom-6 lg:left-6">
             <img className="cursor-pointer" src={Frame} alt="Back" />
           </div>
         </Link>
-        <div>
-          <div className="opacity-100 min-h-screen flex items-center justify-center font-['Roboto'] bg-[#DACBBB]">
-            <div className="bg-[#FFFFFF99] bg-opacity-90 rounded-lg shadow-lg z-[1] p-8 w-full max-w-md">
-              <div className="flex flex-col items-center">
-                <img src={Logo} alt="Logo" className="logo w-56 h-44" />
-              </div>
-              <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                  <label htmlFor="mobileNo" className="block text-sm font-700 text-brown-700 font-Manrope">
-                    Mobile No *
-                  </label>
-                  <input
-                    {...register("mobileNo", {
-                      required: true,
-                      pattern: /^[0-9]{10}$/,
-                      onChange: handleMobileInput
-                    })}
-                    type="text"
-                    id="mobileNo"
-                    name="mobileNo"
-                    placeholder="8669711028"
-                    className="mt-1 font-Manrope block w-full rounded-md border-gray-300 shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50 p-2"
-                  />
-                  {errors.mobileNo && (
-                    <span className="text-red-500 text-sm">
-                      This field is required and must be a 10-digit number
-                    </span>
-                  )}
-                </div>
-                <div>
-                  <label htmlFor="name" className="block text-sm font-700 text-brown-700 font-Manrope">
-                    Customer’s Name*
-                  </label>
-                  <input
-                    {...register("name", {
-                      required: true,
-                      validate: validateFirstLetterCapital
-                    })}
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="John Doe"
-                    value={data.name}
-                    className="mt-1 font-Manrope block w-full rounded-md border-gray-300 shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50 p-2"
-                  />
-                  {errors.name && (
-                    <span className="text-green-600 text-sm">
-                      {errors.name.message || "Please review the Customer’s Name and press again to proceed."}
-                    </span>
-                  )}
-                </div>
-                <div>
-                  <label htmlFor="customerId" className="block text-sm font-700 text-brown-700 font-Manrope">
-                    Customer ID
-                  </label>
-                  <input
-                    {...register("customerId", {required: true})}
-                    type="text"
-                    id="customerId"
-                    name="customerId"
-                    placeholder="ROFC001"
-                    value={data.customerId}
-                    className="mt-1 font-Manrope block w-full rounded-md border-gray-300 shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50 p-2"
-                  />
-                  {errors.customerId && (
-                    <span className="text-green-600 text-sm">
-                      Please review the customer ID and press again to proceed.
-                    </span>
-                  )}
-                </div>
-                <div className="p-2">
-                  <button
-                    type="submit"
-                    className="font-Manrope font-700 w-full bg-[#632E04] text-white py-2 px-4 rounded-md hover:bg-brown-700 focus:outline-none focus:ring-2 focus:ring-brown-500 focus:ring-opacity-50 transition duration-150 ease-in-out">
-                    Proceed for Step 2
-                  </button>
-                </div>
-              </form>
+        <div className="opacity-100 min-h-screen flex items-center justify-center font-['Roboto'] bg-[#DACBBB]">
+          <div className="bg-[#FFFFFF99] bg-opacity-90 rounded-lg shadow-lg z-[1] px-6 w-[514px] h-[564px] flex flex-col items-center">
+            <div className="flex flex-col items-center">
+              <img src={Logo} alt="Logo" className="logo w-[168px] h-[151px]" /> {/* Adjusted logo size */}
             </div>
+
+            <form className="space-y-4 w-full px-6" onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <label
+                  htmlFor="mobileNo"
+                  className="block input-fonts font-Manrope">
+                  Mobile No *
+                </label>
+                <input
+                  {...register("mobileNo", {
+                    required: true,
+                    pattern: /^[0-9]{10}$/,
+                    onChange: handleMobileInput,
+                  })}
+                  type="text"
+                  id="mobileNo"
+                  name="mobileNo"
+                  placeholder="8669711028"
+                  className="mt-1 font-Manrope block input-fields shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50"
+                />
+                {errors.mobileNo && (
+                  <span className="text-red-500 text-sm">
+                    This field is required and must be a 10-digit number
+                  </span>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block input-fonts font-Manrope">
+                  Customer’s Name*
+                </label>
+                <input
+                  {...register("name", {
+                    required: true,
+                    validate: validateFirstLetterCapital
+                  })}
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="John Doe"
+                  value={data.name}
+                  className="mt-1 font-Manrope block input-fields shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50 "
+                />
+                {errors.name && (
+                  <span className="text-red-500 text-sm">
+                    {errors.name.message || "This field is required"}
+                  </span>
+                )}
+              </div>
+
+              <div>
+                <label
+                  htmlFor="customerId"
+                  className="block input-fonts font-Manrope">
+                  Customer ID
+                </label>
+                <input
+                  {...register("customerId", { required: true })}
+                  type="text"
+                  id="customerId"
+                  name="customerId"
+                  placeholder="ROFC001"
+                  value={data.customerId}
+                  className="mt-1 font-Manrope block input-fields shadow-sm focus:border-brown-500 focus:ring focus:ring-brown-500 focus:ring-opacity-50 "
+                />
+                {errors.customerId && (
+                  <span className="text-red-500 text-sm">
+                    This field is required
+                  </span>
+                )}
+              </div>
+
+              <div className="w-full">
+                <button
+                  type="submit"
+                  className="font-Manrope mt-8 ProceedforStep2 bg-[#632E04] text-white rounded-md hover:bg-brown-700 focus:outline-none focus:ring-2 focus:ring-brown-500 focus:ring-opacity-50 transition duration-150 ease-in-out">
+                  Proceed for Step 2
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
