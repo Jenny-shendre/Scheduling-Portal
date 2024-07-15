bcb501cfd5273d5defc7a0d4465dc95612069a7b
Jennifer Shendre <105356506+Jenny-shendre@users.noreply.github.com>
2024-07-15 19:10:30 +0530

    Delete app directory

--- app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import SliderSlice from "../features/Data";
-export const store = configureStore({
-  reducer: {
-    Slider: SliderSlice,
-  },
-});
